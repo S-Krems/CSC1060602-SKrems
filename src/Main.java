50ff38354c2863cbda6c1e55cd9f76fc35af6241
S-Krems <sethkrems123@outlook.com>
2024-02-26 18:51:17 -0700

    ArrayReversal assignment

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[100];
+        int index = 0;
+        while (true) {
+            System.out.println("Enter values (-1 to stop)");
+            int num = sc.nextInt();
+            if (num == -1) {
+                break;cd
+            }
+            numbers[index] = num;
+            index++;
+        }
+        for (int i = index - 1; i >= 0; i--) {
+            System.out.println(numbers[i]);
+        }
+    }
+}
